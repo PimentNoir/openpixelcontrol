39b5ef5d6cb27770cf2b62524a35e3d2fefdb6ea
Jérôme Benoit <jerome.benoit@grenouille.com>
2014-02-03 03:48:30 +0100

    Fix the led order for the zigzag wiring inside the PCB.
    Signed-off-by: Jérôme Benoit <jerome.benoit@grenouille.com>

--- layouts/grid.py
+++ layouts/grid.py
@@ -5,7 +5,7 @@ grid_size = 11 #
 
 lines = []
 for c in range(-(grid_size-2)/2, (grid_size+2)/2):
-    rs = [reversed(range(grid_size)), range(grid_size)][c % 2]
+    rs = [range(grid_size), reversed(range(grid_size))][c % 2]
     for r in rs:
         lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
                      (c*spacing, 0, (r-grid_size/2)*spacing))
